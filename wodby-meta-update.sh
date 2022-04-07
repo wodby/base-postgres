5147b68825a4fb3f28d7c488ceeb31f9949680d6
Chingis Sandanov <chingis@wodby.com>
2022-04-07 14:16:58 +0800

    Fix versions update script

--- wodby-meta-update.sh
+++ wodby-meta-update.sh
@@ -9,15 +9,12 @@ for ver in "${versions[@]}"; do
   # Change basic image.
   sed -i '/FROM alpine/i\ARG BASE_IMAGE_TAG\n' ${ver}/alpine/Dockerfile.wodby
   sed -i 's/FROM alpine.*/FROM wodby\/alpine:${BASE_IMAGE_TAG}/' ${ver}/alpine/Dockerfile.wodby
-  # Alpine 3.13 has older version of llvm.
-  sed -i 's/llvm11/llvm10/' ${ver}/alpine/Dockerfile.wodby
 
   fullVersion=$(grep -oP '(?<=^ENV PG_VERSION )([0-9\.]+)' ${ver}/alpine/Dockerfile.wodby)
   minorVersion=$(echo "${fullVersion}" | sed -E 's/.[0-9]+$$//')
 
   # Update gh workflow
-  sed -i -E "s/(version): '${minorVersion//\./\\.}\.[0-9]+'/\1: '${fullVersion}'/" .github/workflows/workflow.yml
-  sed -i -E "s/(tags): ${minorVersion//\./\\.}\.[0-9]+/\1: ${fullVersion}/" .github/workflows/workflow.yml
+  sed -i -E "s/(POSTGRES${minorVersion//\./}): '${minorVersion//\./\\.}\.[0-9]+'/\1: '${fullVersion}'/" .github/workflows/workflow.yml
   # Update README.
-  sed -i -E "s/\`${minorVersion//\./\\.}\.[0-9]+\`/\`${fullVersion}\`/" README.md
+  sed -i -E "s/\`${minorVersion//\./\\.}\.[0-9]+\`/\`${fullVersion}\`/g" README.md
 done
